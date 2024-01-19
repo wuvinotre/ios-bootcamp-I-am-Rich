582c5a2b73680d823c76b5afdf329cacf826e5ab
Vinicius <ewerton.viniciusturco@gmail.com>
2024-01-19 11:45:03 -0300

    first commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by vuinishiusu on 09/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
